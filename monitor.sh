d410a53ebbbe207f6c6f9c96e3b1b18ab9b31dfb
Lucas Bugnazet <lucasbugnazet@gmail.com>
2018-05-09 10:03:02 +0200

    Fixed typo

--- monitor.sh
+++ monitor.sh
@@ -9,7 +9,7 @@ for (( ; ; )); do
     curl $URL -L --compressed -s > new.html
     DIFF_OUTPUT="$(diff new.html old.html)"
     if [ "0" != "${#DIFF_OUTPUT}" ]; then
-        echo "<p>Visit it at <a href=" $URL ">" "$URL</a></p><p>h1>Diff :</h1><p>" $DIFF_OUTPUT "<p>" | mail -s "Web page changed" -a "Content-type: text/html;" $USERNAME
+        echo "<p>Visit it at <a href=" $URL ">" "$URL</a></p><p><h1>Diff :</h1><p>" $DIFF_OUTPUT "<p>" | mail -s "Web page changed" -a "Content-type: text/html;" $USERNAME
         sleep 1800
     fi
 done
