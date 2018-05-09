796ab22e37fa2a55213aa67c63ff8d327ac26b75
Lucas Bugnazet <lucasbugnazet@gmail.com>
2018-05-09 10:16:10 +0200

    Better outputing

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@ URL="http://thepage.com/that/I/want/to/monitor"
 for (( ; ; )); do
     mv new.html old.html 2> /dev/null
     curl $URL -L --compressed -s > new.html
-    DIFF_OUTPUT="$(diff new.html old.html)"
+    DIFF_OUTPUT="$(diff new.html old.html -u)"
     if [ "0" != "${#DIFF_OUTPUT}" ]; then
         echo "<p>Visit it at <a href=" $URL ">" "$URL</a></p><p><h1>Diff :</h1><p>" $DIFF_OUTPUT "<p>" | mail -s "Web page changed" -a "Content-type: text/html;" $USERNAME
         sleep 1800
